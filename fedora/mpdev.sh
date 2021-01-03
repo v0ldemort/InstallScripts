090c4a12c99eead3f51585cef7caf5e22d42e71c
voldemort <65954696+v0ldemort@users.noreply.github.com>
2021-01-03 23:23:33 +0530

    Update mpdev.sh

--- fedora/mpdev.sh
+++ fedora/mpdev.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-mkdir bin
+mkdir $HOME/bin
 docker pull gcr.io/cloud-marketplace-tools/k8s/dev
 BIN_FILE="$HOME/bin/mpdev"
 export BIN_FILE="$HOME/bin/mpdev"
 docker run   gcr.io/cloud-marketplace-tools/k8s/dev cat /scripts/dev > "$BIN_FILE"
-cd $HOME
 source ~/.bash_profile
 chmod +x "$BIN_FILE"
 mpdev
