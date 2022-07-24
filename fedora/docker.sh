dacecc1fd43fb89382013bf11e6c138e1c8301d7
voldemort <65954696+v0ldemort@users.noreply.github.com>
2022-07-24 14:16:02 +0530

    Update docker.sh

--- fedora/docker.sh
+++ fedora/docker.sh
@@ -27,3 +27,18 @@ sudo gpasswd -a ${USER} docker && sudo systemctl restart docker
 
 echo "Docker has been installed"
 
+######
+
+# Optional step to Install docker-compose
+
+read -p "Do you want to install docker-copose as well [yes/no]: " var1
+
+if [ "$var1" = "yes" ]
+   then
+      sudo curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+      sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+      docker-compose --version
+      echo "Docker-Compose has been installed successfully"
+    else
+      exit 0
+fi
